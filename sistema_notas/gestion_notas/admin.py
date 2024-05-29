140980f996a1e5865632a03919c29d06c77a8341
Anthony <acahuib@unsa.edu.pe>
2024-05-29 12:15:36 -0500

    Se elimina el proyecto

--- sistema_notas/gestion_notas/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Alumno, Curso, NotasAlumnosPorCurso
-
-admin.site.register(Alumno)
-admin.site.register(Curso)
-admin.site.register(NotasAlumnosPorCurso)
-
