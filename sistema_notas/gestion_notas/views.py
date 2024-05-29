d4d3fd53759165fe24664fc17d90994f93856cbf
Anthony <acahuib@unsa.edu.pe>
2024-05-29 10:38:45 -0500

    Se agrega para las vistas la funcion de listar_alumnos y nuevo_alumno

--- sistema_notas/gestion_notas/views.py
+++ sistema_notas/gestion_notas/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AlumnoForm, CursoForm, NotasAlumnosPorCursoForm
+from .models import Alumno, Curso, NotasAlumnosPorCurso
 
-# Create your views here.
+def listar_alumnos(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'listar_alumnos.html', {'alumnos': alumnos})
+
+def nuevo_alumno(request):
+    if request.method == "POST":
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_alumnos')
+    else:
+        form = AlumnoForm()
+    return render(request, 'nuevo_alumno.html', {'form': form})
+
