9606bac8fc9010d5c7d36aa681927b90cd20c52e
Anthony <acahuib@unsa.edu.pe>
2024-05-29 12:13:37 -0500

    Se elimina todo el antiguo proyecto para poder empezar de nuevo con otro poryecto con le mismo contenido pero mas ordenado

--- sistema_notas/gestion_notas/views.py
+++ sistema_notas/gestion_notas/views.py
@@ -4,17 +4,22 @@ from .models import Alumno, Curso, NotasAlumnosPorCurso
 
 def listar_alumnos(request):
     alumnos = Alumno.objects.all()
-    return render(request, 'listar_alumnos.html', {'alumnos': alumnos})
+    return render(request, 'gestion_notas/listar_alumnos.html', {'alumnos': alumnos})
 
 def nuevo_alumno(request):
     if request.method == "POST":
         form = AlumnoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listar_alumnos')
+            return redirect('listar_alumnos')  # Corregir la redirección
     else:
         form = AlumnoForm()
-    return render(request, 'nuevo_alumno.html', {'form': form})
+    return render(request, 'gestion_notas/nuevo_alumno.html', {'form': form})
+
+def listar_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'gestion_notas/listar_cursos.html', {'cursos': cursos})
+
 def nuevo_curso(request):
     if request.method == "POST":
         form = CursoForm(request.POST)
@@ -23,11 +28,11 @@ def nuevo_curso(request):
             return redirect('listar_cursos')
     else:
         form = CursoForm()
-    return render(request, 'nuevo_curso.html', {'form': form})
+    return render(request, 'gestion_notas/nuevo_curso.html', {'form': form})
 
 def listar_notas(request):
     notas = NotasAlumnosPorCurso.objects.all()
-    return render(request, 'listar_notas.html', {'notas': notas})
+    return render(request, 'gestion_notas/listar_notas.html', {'notas': notas})
 
 def nueva_nota(request):
     if request.method == "POST":
@@ -37,4 +42,5 @@ def nueva_nota(request):
             return redirect('listar_notas')
     else:
         form = NotasAlumnosPorCursoForm()
-    return render(request, 'nueva_nota.html', {'form': form})
+    return render(request, 'gestion_notas/nueva_nota.html', {'form': form})
+
